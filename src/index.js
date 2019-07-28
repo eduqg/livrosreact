d4ef761b33be73cd0f8444ca7d3c9174508db535
eduqg <eduardoqgomes@gmail.com>
2019-07-27 22:32:06 -0300

    New layout pure css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+// Para criar novos elementos
 import React from 'react';
+// Classe que cria elementos em memoria para simular DOM do navegador
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// Importar modulo App
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Posso colocar <App /> também
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
