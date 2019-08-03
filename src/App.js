da32c94fbc16f05dc5a06425856df0780c8cc77c
eduqg <eduardoqgomes@gmail.com>
2019-08-02 21:02:33 -0300

    Post de autores funcionando

--- src/App.js
+++ src/App.js
@@ -11,20 +11,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lista: []
+      lista: [],
+      nome:'',
+      email:'',
+      password:''
     };
+    //Em enviaForm preciso saber os dados que serão usados. Serão usados os dados do componente enviado (this)
+    //this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setPassword = this.setPassword.bind(this);
   }
 
   componentDidMount() {
-    console.log("didMount");
     $.ajax({
       url: "http://localhost:9090/autors",
       dataType: 'json',
       success: function (resposta) {
-        console.log("chegou a resposta");
         this.setState({ lista: resposta });
       }.bind(this)
     });
+  }
+
+  enviaForm(eventoDisparado){
+    // Previne que o evento não continue sendo propagado pelo DOM 
+    // Sem esse preventDefault ocorre um erro no post de autores
+    eventoDisparado.preventDefault();
+    console.log("Dados sendo enviados");
+    $.ajax({
+      url:'http://localhost:9090/autors',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data:JSON.stringify({nome:this.state.nome,email:this.state.email,password:this.state.password}),
+      success: function(resposta){
+        console.log("Enviado com sucesso");
+      },
+      error: function(erro){
+        console.log("Deu erro");
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value})
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value})
+  }
+
+  setPassword(evento){
+    this.setState({password:evento.target.value})
   }
 
   render() {
@@ -53,18 +91,18 @@ class App extends React.Component {
 
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post" >
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" defaultValue="" />
+                  <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}  />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" defaultValue="" />
+                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}  />
                 </div>
                 <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" />
+                  <label htmlFor="password">Senha</label>
+                  <input id="password" type="password" name="password" value={this.state.password} onChange={this.setPassword} />
                 </div>
                 <div className="pure-control-group">
                   <label></label>
